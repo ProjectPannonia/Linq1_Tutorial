21dbcb5aa60dfcfc0f440228478234037889d094
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-03-22 22:20:32 +0100

    Basic functions of linq

--- Linq1/Program.cs
+++ Linq1/Program.cs
@@ -10,6 +10,39 @@ namespace Linq1
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //OddNumbers(numbers);
+            WholeNumbers(numbers);
+            Console.ReadKey();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd numbers: ");
+
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+            Console.WriteLine(oddNumbers);
+
+            foreach(int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void WholeNumbers(int[] numbers)
+        {
+            Console.WriteLine("Whole numbers: ");
+
+            IEnumerable<int> wholeNumbers = from number in numbers where number % 2 == 0 select number;
+
+            Console.WriteLine(wholeNumbers);
+
+            foreach(int i in wholeNumbers)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
